d59f1bf04f02be81734ed384688aff4cd78061e2
kayveedeeoh <kayveedeeoh@gmail.com>
2018-12-11 10:49:59 +0200

    test finished

--- linear_regression/cricket_chirps_temperature.py
+++ linear_regression/cricket_chirps_temperature.py
@@ -7,7 +7,7 @@ Y = temperature in degrees Fahrenheit
 Reference: The Song of Insects by Dr.G.W. Pierce, Harvard College Press
 https://college.cengage.com/mathematics/brase/understandable_statistics/7e/students/datasets/slr/frames/frame.html
 
-The function of the program is to predict how many cricket chirps/sec will result from a particular temperature."""
+The function of the program is to predict how many cricket chirps/sec will result from a given temperature."""
 
 import pandas as pd
 import numpy as np
@@ -21,7 +21,7 @@ dataset = pd.read_excel("https://college.cengage.com/mathematics/brase/understan
 X = dataset.iloc[:, 0].values.reshape(-1, 1)  # Attributes
 y = dataset.iloc[:, 1].values.reshape(-1, 1)  # Labels
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0, random_state=0)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
 regressor = LinearRegression()
 regressor.fit(X_train, y_train)
 
@@ -31,11 +31,19 @@ b = regressor.intercept_
 stop = dataset.max()[0]
 start = dataset.min()[0]
 
+y_prediction = regressor.predict(X_test)
+y_prediction = [y for x in y_prediction for y in x]
+X_test = [y for x in X_test for y in x]
+
+df = pd.DataFrame({'Test': list(X_test), 'Prediction': list(y_prediction)})
+
 # Plot the data
-dataset.plot(x="X", y="Y", style='o')
+ax = dataset.plot(x="X", y="Y", style='o')
+x = np.linspace(start, stop)
+plt.plot(x, m * x + b)
+df.plot(x='Test', y='Prediction', style='r+', ax=ax)
+
 plt.title("Cricket Chirps Vs. Temperature")
 plt.xlabel("Chirps/sec")
 plt.ylabel("Temperature in degrees Fahrenheit")
-x = np.linspace(start, stop)
-plt.plot(x, m * x + b)
 plt.show()
